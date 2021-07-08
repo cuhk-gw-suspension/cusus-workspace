6b2e2147780e60f65aa95161fa127d941e32a986
Lam Yee Ching <lamyc627@gmail.com>
2021-07-08 18:14:16 +0800

    added moveTo func

---
+++ stepper/new_arduino_stepper/MyStepper.cpp
@@ -0,0 +1,97 @@
+include "MyStepper.h"
+
+Stepper::Stepper(uint8_t pul_pin, uint8_t dir_pin, bool enable = true){
+    _pul_pin = pul_pin;
+    _dir_pin = dir_pin;
+    _enable = enable;
+    
+    _currentPos = 0;
+    _direction = 0;
+
+    initOutputPins(_pul_pin);
+    initOutputPins(_dir_pin);
+}
+
+void Stepper::moveTo(long absolute){
+    _targetPos = absolute;
+    int displacement = _targetPos - _currentPos;
+    if (displacement > 0)
+        setDirection(HIGH);
+    else if (displacement < 0)
+        setDirection(LOW);
+}
+
+void Stepper::setDirection(bool direction){
+    _direction = direction;
+    setOutputPins(_dir_pin, _direction);
+}
+
+void Stepper::run(){
+    if (_currentPos != _targetPos){
+        step();
+        _currentPos += _direction ? 1 : -1;
+    }
+}
+
+void Stepper::step(){
+    setOutputPins(2, HIGH);
+    delayMicroseconds(5); // need to replace by other delay method
+    setOutputPins(2, LOW);
+}
+
+inline long Stepper::getPosition(){
+    return _currentPos;
+}
+
+inline long distanceToGo(){
+    return _targetPos - _currentPos;
+}
+
+char Stepper::locatePinSector(uint8_t pin){ 
+    if (pin < 8)
+        return 'D';
+    else if (pin < 14)
+        return 'B';
+}
+
+void Stepper::initOutputPins(uint8_t pin) {
+    volatile uint8_t *addrToDataDirectionRegister;
+    switch(locatePinSector(pin))
+    {
+    case 'D':
+        addrToDataDirectionRegister = &DDRD;
+        break;
+
+    case 'B':
+        pin -= 8;
+        addrToDataDirectionRegister = &DDRB;
+        break;
+    }
+    
+    *addrToDataDirectionRegister |= (1 << pin); // initialize pin 
+}
+
+void Stepper::setOutputPins(uint8_t pin, bool state){
+    // set outputPin as HIGH or LOW.
+    volatile uint8_t *addrToDataRegister;
+
+    switch(locatePinSector(pin))
+    {
+    case 'D':
+        addrToDataRegister = &PORTD;
+        break;
+
+    case 'B':
+        pin -= 8;
+        addrToDataRegister = &PORTB;
+        break;
+    }
+
+    if (state)
+        *addrToDataRegister |= (1 << pin); // wrtie HIGH to the pin
+    else
+        *addrToDataRegister &= ~(1 << pin); // write LOW to the pin
+}
+
+
+
--- stepper/new_arduino_stepper/new_arduino_stepper.ino
+++ stepper/new_arduino_stepper/new_arduino_stepper.ino
@@ -1,85 +1,21 @@
 /* My self defined arduino stepper script
 
 */
+#include "MyStepper.h"
 
-int step_per_rev = 25600;
-int currentPos = 0;
+unsigned long step_per_rev = 25600;
+
+Stepper stepper1(2, 3); //init pul pin=2, dir pin=3
 
 void setup() {
   // put your setup code here, to run once:
-    
-    for (int i = 2; i < 4; i++)
-        initOutputPins(i);
-    setOutputPins(3, HIGH);
+   stepper1.moveTo(step_per_rev*3); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    if (currentPos <= step_per_rev*3){
-        run();
-        currentPos += 1;
-    }
-    else{
-        currentPos = 0;
-        setOutputPins(3, LOW);
-    }
+    // put your main code here, to run repeatedly:
+    if (stepper1.distanceToGo() == 0)
+        stepper1.moveTo(stepper1.getPosition());    
+    stepper1.run();
 }
-
-void moveTo(){
-    
-}
-
-void run(){
-    setOutputPins(2, HIGH);
-    delayMicroseconds(5); // need to replace by other delay method
-    setOutputPins(2, LOW);
-}
-
-char _locate_pin_sector(uint8_t pin){ 
-    if (pin < 8)
-        return 'D';
-    else if (pin < 14)
-        return 'B';
-}
-
-void initOutputPins(uint8_t pin) {
-    volatile uint8_t *addrToDataDirectionRegister;
-    switch(_locate_pin_sector(pin))
-    {
-    case 'D':
-        addrToDataDirectionRegister = &DDRD;
-        break;
-
-    case 'B':
-        pin -= 8;
-        addrToDataDirectionRegister = &DDRB;
-        break;
-    }
-    
-    *addrToDataDirectionRegister |= (1 << pin); // initialize pin 
-}
-
-void setOutputPins(uint8_t pin, bool state){
-    // set outputPin as HIGH or LOW.
-    volatile uint8_t *addrToDataRegister;
-
-    switch(_locate_pin_sector(pin))
-    {
-    case 'D':
-        addrToDataRegister = &PORTD;
-        break;
-
-    case 'B':
-        pin -= 8;
-        addrToDataRegister = &PORTB;
-        break;
-    }
-
-    if (state)
-        *addrToDataRegister |= (1 << pin); // wrtie HIGH to the pin
-    else
-        *addrToDataRegister &= ~(1 << pin); // write LOW to the pin
-}
-
-
 
