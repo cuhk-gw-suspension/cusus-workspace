87d4ce905519b2256cf37254ca4b2627c8c263b4
Lam Yee Ching <lamyc627@gmail.com>
2021-07-14 20:27:33 +0800

    added geophone test scipt

---
+++ arduino/geophone/geophone.ino
@@ -0,0 +1,15 @@
+int analogPin = A4;
+
+int val;
+
+void setup() { 
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(analogPin);
+  Serial.println(val);
+
+}
